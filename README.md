8c239fe7a8340d781d6e108bfa3a10812d61d0eb
Aleksandar Markicevic <aleksandar.markicevic@avssolutions.rs>
2019-11-29 21:45:11 +0100

    fix README.md and fix app source file indentation

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 ## Multi-environment scalable microservices system with centralized logging
 
-Ngingx to route all the incoming trafic.
-Frontend and backend application with minimal codebase to demonstate application logging.
-EFK stack for collecting, storing and visualizing logs.
+Nginx to route all the incoming trafic.<br/>
+Frontend and backend application with minimal codebase to demonstate application logging.<br/>
+EFK stack for collecting, storing and visualizing logs.<br/>
 
 ![Infra diagram](infra-diagram.png)
 
@@ -11,17 +11,17 @@ EFK stack for collecting, storing and visualizing logs.
 
 
 - Gateway
-	- Debian 10
-	- Nginx 1.17.5
+    - Debian 10
+    - Nginx 1.17.5
 
 - Applications
-	- Ubuntu 16.04
-	- GoLang 1.6.2
+    - Ubuntu 16.04
+    - GoLang 1.6.2
 
 - EFK
-	- Elasticsearch 6.3.0
-	- Fluentd 1.2
-	- Kibana 6.3.0
+    - Elasticsearch 6.3.0
+    - Fluentd 1.2
+    - Kibana 6.3.0
 
 ## Prerequisites
 
@@ -32,20 +32,20 @@ EFK stack for collecting, storing and visualizing logs.
 
 ## Installation
 
-Clone the project
+**Clone the project**
 ```
 $ git clone https://github.com/aleksmark/centralized-demo.git
 $ cd centralized-demo
 ```
 
-Update hosts file (required to run with sudo user)
+**Update hosts file (required to run with sudo user)**
 ```
 $ echo "127.0.0.1 development.central.demo" >> /etc/hosts
 $ echo "127.0.0.1 staging.central.demo" >> /etc/hosts
 $ echo "127.0.0.1 kibana.central.demo" >> /etc/hosts
 ```
 
-Build the docker environment
+**Build the docker environment**
 ```
 $ docker-compose up -d --build \
     --scale backend-development=3 \
@@ -54,7 +54,7 @@ $ docker-compose up -d --build \
     --scale frontend-staging=3
 ```
 
-Create index pattern
+**Create index pattern**
 
 Follow the [documentation](https://www.elastic.co/guide/en/kibana/6.3/tutorial-define-index.html#tutorial-define-index) to create index pattern with name `fluend`
 
@@ -62,6 +62,6 @@ Follow the [documentation](https://www.elastic.co/guide/en/kibana/6.3/tutorial-d
 
 Access the services on your local machine
 
-http://development.central.demo
-http://staging.central.demo
-http://kibana.central.demo
+http://development.central.demo<br/>
+http://staging.central.demo<br/>
+http://kibana.central.demo<br/>
--- frontend/src/main.go
+++ frontend/src/main.go
@@ -1,124 +1,124 @@
 package main
 
 import (
-	"encoding/json"
-	"flag"
-	"fmt"
-	"html/template"
-	"net/http"
-	"os"
+    "encoding/json"
+    "flag"
+    "fmt"
+    "html/template"
+    "net/http"
+    "os"
 )
 
 var (
-	listenPort  string
-	backendHost = os.Getenv("BACKEND_HOST")
-	backendPort = os.Getenv("BACKEND_PORT")
+    listenPort  string
+    backendHost = os.Getenv("BACKEND_HOST")
+    backendPort = os.Getenv("BACKEND_PORT")
 )
 
 type PageVariables struct {
-	Hostname string
-	Message  string
-	Env      string
+    Hostname string
+    Message  string
+    Env      string
 }
 
 type LogRecord struct {
-	Level       string `json:"level"`
-	Message     string `json:"message"`
-	Environment string `json:"environment"`
-	Application string `json:"application"`
-	Hostname    string `json:"hostname"`
+    Level       string `json:"level"`
+    Message     string `json:"message"`
+    Environment string `json:"environment"`
+    Application string `json:"application"`
+    Hostname    string `json:"hostname"`
 }
 
 func main() {
-	flag.StringVar(&listenPort, "listen-port", "80", "server listen port")
-	flag.Parse()
+    flag.StringVar(&listenPort, "listen-port", "80", "server listen port")
+    flag.Parse()
 
-	myLog("info", "Server is starting on port "+listenPort)
+    myLog("info", "Server is starting on port "+listenPort)
 
-	http.HandleFunc("/index", serveIndexPage)
-	http.HandleFunc("/login", serveLoginPage)
-	http.HandleFunc("/vote", serveVotePage)
+    http.HandleFunc("/index", serveIndexPage)
+    http.HandleFunc("/login", serveLoginPage)
+    http.HandleFunc("/vote", serveVotePage)
 
-	http.ListenAndServe(":"+listenPort, nil)
+    http.ListenAndServe(":"+listenPort, nil)
 }
 
 func serveIndexPage(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "index page")
+    renderTemplate(w, "index page")
 
-	myLog("info", "User landed on index page")
+    myLog("info", "User landed on index page")
 }
 
 func serveLoginPage(w http.ResponseWriter, r *http.Request) {
-	username, _ := r.URL.Query()["username"]
-	password, _ := r.URL.Query()["password"]
+    username, _ := r.URL.Query()["username"]
+    password, _ := r.URL.Query()["password"]
 
-	resp, _ := http.Get("http://" + backendHost + ":" + backendPort + "/login?username=" + username[0] + "&password=" + password[0])
+    resp, _ := http.Get("http://" + backendHost + ":" + backendPort + "/login?username=" + username[0] + "&password=" + password[0])
 
-	if resp.StatusCode == 200 {
-		renderTemplate(w, "Welcome User "+username[0])
+    if resp.StatusCode == 200 {
+        renderTemplate(w, "Welcome User "+username[0])
 
-		myLog("info", "User "+username[0]+" logged in")
-	} else {
-		renderTemplate(w, "Wrong username and password")
+        myLog("info", "User "+username[0]+" logged in")
+    } else {
+        renderTemplate(w, "Wrong username and password")
 
-		myLog("error", "Invalid login attempt")
-	}
+        myLog("error", "Invalid login attempt")
+    }
 }
 
 func serveVotePage(w http.ResponseWriter, r *http.Request) {
-	resp, _ := http.Get("http://" + backendHost + ":" + backendPort + "/vote")
+    resp, _ := http.Get("http://" + backendHost + ":" + backendPort + "/vote")
 
-	if resp.StatusCode == 200 {
-		renderTemplate(w, "You voted successfully")
+    if resp.StatusCode == 200 {
+        renderTemplate(w, "You voted successfully")
 
-		myLog("info", "User voted")
-	} else {
-		renderTemplate(w, "Vote failed")
+        myLog("info", "User voted")
+    } else {
+        renderTemplate(w, "Vote failed")
 
-		myLog("error", "User vote failed")
-	}
+        myLog("error", "User vote failed")
+    }
 }
 
 func renderTemplate(w http.ResponseWriter, message string) {
-	indexPageVars := PageVariables{
-		Hostname: getHostname(),
-		Env:      os.Getenv("ENV"),
-		Message:  message,
-	}
+    indexPageVars := PageVariables{
+        Hostname: getHostname(),
+        Env:      os.Getenv("ENV"),
+        Message:  message,
+    }
 
-	t, err := template.ParseFiles("index.html")
-	if err != nil {
-		myLog("error", "Template parsing error: "+err.Error())
-	}
+    t, err := template.ParseFiles("index.html")
+    if err != nil {
+        myLog("error", "Template parsing error: "+err.Error())
+    }
 
-	err = t.Execute(w, indexPageVars)
-	if err != nil {
-		myLog("error", "Template executing error: "+err.Error())
-	}
+    err = t.Execute(w, indexPageVars)
+    if err != nil {
+        myLog("error", "Template executing error: "+err.Error())
+    }
 
-	serialized, _ := json.Marshal(indexPageVars)
-	myLog("info", "Template rendered with data: "+string(serialized))
+    serialized, _ := json.Marshal(indexPageVars)
+    myLog("info", "Template rendered with data: "+string(serialized))
 }
 
 func getHostname() string {
-	name, err := os.Hostname()
-	if err != nil {
-		panic(err)
-	}
+    name, err := os.Hostname()
+    if err != nil {
+        panic(err)
+    }
 
-	return name
+    return name
 }
 
 func myLog(level string, message string) {
-	logRecord := LogRecord{
-		Level:       level,
-		Message:     message,
-		Environment: os.Getenv("ENV"),
-		Application: os.Getenv("APP"),
-		Hostname:    getHostname(),
-	}
+    logRecord := LogRecord{
+        Level:       level,
+        Message:     message,
+        Environment: os.Getenv("ENV"),
+        Application: os.Getenv("APP"),
+        Hostname:    getHostname(),
+    }
 
-	serialized, _ := json.Marshal(logRecord)
+    serialized, _ := json.Marshal(logRecord)
 
-	fmt.Println(string(serialized))
+    fmt.Println(string(serialized))
 }
